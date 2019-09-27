8e5c3052dd197c4cfea88eee7124c6f738665241
phongtiger <phongken94@gmail.com>
2019-09-27 15:37:44 +0700

    hien thi danh sach

--- src/main/java/com/codegym/controller/CustomerController.java
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -1,25 +1,18 @@
 package com.codegym.controller;
 
-import com.codegym.model.Customer;
 import com.codegym.service.CustomerService;
 import com.codegym.service.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
 
 @Controller
 public class CustomerController{
     private CustomerService customerService = new CustomerServiceImpl();
 
     @GetMapping("/")
-    String showForm(){
+    String showForm(Model model){
+        model.addAttribute("customers",customerService.findAll());
         return "list";
     }
 }
